437ba1f39a95ec85ecaa3edb33ac7e5420bbe584
UnknownIAbug <rchrd.btlh@gmail.com>
2026-07-17 18:50:46 -0400

    Update database.py

--- database/database.py
+++ database/database.py
@@ -1,1 +1,22 @@
+import sqlite3
 
+def conectar():
+    return sqlite3.connect("garimpofree.db")
+
+
+def criar_banco():
+    conexao = conectar()
+    cursor = conexao.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS oportunidades(
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        produto TEXT,
+        descricao TEXT,
+        valor TEXT,
+        link TEXT
+    )
+    """)
+
+    conexao.commit()
+    conexao.close()
