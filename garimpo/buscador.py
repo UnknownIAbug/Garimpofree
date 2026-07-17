3be62c91aed8505f62e2822b4b14da2c4f9e3030
UnknownIAbug <rchrd.btlh@gmail.com>
2026-07-17 19:14:18 -0400

    Create buscador.py

---
+++ garimpo/buscador.py
@@ -0,0 +1,39 @@
+# garimpo/buscador.py
+
+def buscar_oportunidades():
+
+    oportunidades = [
+
+        {
+            "titulo": "iPhone 14 Pro Max",
+            "descricao": "Venda urgente. Aparelho muito abaixo do mercado.",
+            "categoria": "Celulares",
+            "cidade": "Corumbá",
+            "fonte": "Marketplace",
+            "link": "https://exemplo.com/1",
+            "preco": 2200
+        },
+
+        {
+            "titulo": "Notebook Dell Inspiron",
+            "descricao": "Doação para retirada imediata.",
+            "categoria": "Informática",
+            "cidade": "Corumbá",
+            "fonte": "Grupo Facebook",
+            "link": "https://exemplo.com/2",
+            "preco": 0
+        },
+
+        {
+            "titulo": "Relógio Garmin",
+            "descricao": "Produto novo pela metade do preço.",
+            "categoria": "Relógios",
+            "cidade": "Ladário",
+            "fonte": "OLX",
+            "link": "https://exemplo.com/3",
+            "preco": 900
+        }
+
+    ]
+
+    return oportunidades
