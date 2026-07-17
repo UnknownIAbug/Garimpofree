08f261d1dfd72bd60873d31f7daf8a4080d53f87
UnknownIAbug <rchrd.btlh@gmail.com>
2026-07-17 18:58:03 -0400

    Create config.py

---
+++ config/config.py
@@ -0,0 +1,25 @@
+CIDADES = [
+    "Corumbá",
+    "Ladário"
+]
+
+PALAVRAS_CHAVE = [
+    "doação",
+    "gratuito",
+    "grátis",
+    "desapego",
+    "retirada",
+    "mudança"
+]
+
+CATEGORIAS = [
+    "geladeira",
+    "fogão",
+    "sofá",
+    "bicicleta",
+    "televisão",
+    "celular",
+    "máquina de lavar",
+    "guarda-roupa",
+    "ar-condicionado"
+]
