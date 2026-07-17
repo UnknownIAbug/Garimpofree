052cac1bb432d6257621931d55683b709516a7a4
UnknownIAbug <rchrd.btlh@gmail.com>
2026-07-17 19:04:10 -0400

    Update config.py

--- config/config.py
+++ config/config.py
@@ -1,6 +1,18 @@
+# config/config.py
+
 CIDADES = [
     "Corumbá",
-    "Ladário"
+    "Ladário",
+    "Campo Grande"
+]
+
+FONTES = [
+    "Facebook Marketplace",
+    "OLX",
+    "Mercado Livre",
+    "Enjoei",
+    "Grupos de Facebook",
+    "Grupos de Telegram"
 ]
 
 PALAVRAS_CHAVE = [
@@ -8,18 +20,52 @@ PALAVRAS_CHAVE = [
     "gratuito",
     "grátis",
     "desapego",
+    "urgente",
+    "mudança",
     "retirada",
-    "mudança"
+    "queima de estoque",
+    "liquidação",
+    "encerrando atividades",
+    "preço baixo",
+    "abaixo do mercado",
+    "lote",
+    "estoque",
+    "promoção",
+    "imperdível"
 ]
 
 CATEGORIAS = [
-    "geladeira",
-    "fogão",
-    "sofá",
-    "bicicleta",
-    "televisão",
-    "celular",
-    "máquina de lavar",
-    "guarda-roupa",
-    "ar-condicionado"
+    "eletrônicos",
+    "celulares",
+    "informática",
+    "notebooks",
+    "games",
+    "videogames",
+    "móveis",
+    "eletrodomésticos",
+    "ferramentas",
+    "relógios",
+    "roupas",
+    "calçados",
+    "semijoias",
+    "joias",
+    "bicicletas",
+    "motos",
+    "carros",
+    "peças automotivas",
+    "material de construção",
+    "máquinas",
+    "equipamentos comerciais",
+    "instrumentos musicais",
+    "colecionáveis",
+    "antiguidades",
+    "livros",
+    "brinquedos",
+    "artigos infantis",
+    "produtos pet",
+    "esportes",
+    "pesca",
+    "camping",
+    "decoração",
+    "utensílios domésticos"
 ]
